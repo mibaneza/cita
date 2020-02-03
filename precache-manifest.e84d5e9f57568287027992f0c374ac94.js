560f14df3bba79d7ba821a393fcba70309523a88
mibaneza <mibaneza@ucvvirtual.edu.pe>
2020-02-02 23:51:07 -0500

    fast
    go

---
+++ precache-manifest.e84d5e9f57568287027992f0c374ac94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bacde60b3870cd35f5b5bdea2475757d",
+    "url": "/cita/index.html"
+  },
+  {
+    "revision": "57b29417fa6308c0b1d6",
+    "url": "/cita/static/css/main.5099c877.chunk.css"
+  },
+  {
+    "revision": "826bb55a7c72d8f1c05d",
+    "url": "/cita/static/js/2.4146f786.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cita/static/js/2.4146f786.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57b29417fa6308c0b1d6",
+    "url": "/cita/static/js/main.12c3cd8d.chunk.js"
+  },
+  {
+    "revision": "3cd032c4ec8b46da5a05",
+    "url": "/cita/static/js/runtime-main.f95bb31d.js"
+  }
+]);
